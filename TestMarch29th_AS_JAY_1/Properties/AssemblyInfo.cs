454989e9832b9bd0658fb4c7b5755506e032b4e8
SKYLINE2\JoachimAY <joachim.ally@skyline.be>
2023-03-29 10:43:08 +0200

    Add project files.

---
+++ TestMarch29th_AS_JAY_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestMarch29th_AS_JAY_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("TestMarch29th_AS_JAY_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("802F19EC-54D8-435D-AFEA-8FB37503E32B")]
+[assembly: AssemblyVersion("1.0.0.0")]
